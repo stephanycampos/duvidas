f11809ab22df853998009ab74d8843524f7e0e15
Stephany Campos <stephanyenne@gmail.com>
2025-05-30 11:31:17 -0300

    Add files via upload

---
+++ gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less:{
+            development: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            }
+        }
+    })
+
+    grunt.registerTask('OlaGrunt', function() {
+        const done = this.async();
+        setTimeout(function() {
+            console.log('Olá Grunt');
+            done();
+        }, 3000);
+    })
+
+    grunt.loadNpmTask('grunt-contrib-less');
+    grunt.registerTask('default', ['less']);
+}
